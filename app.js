3dd5482a65d748807c79e0350cf3a1fd4b718c58
Evan Wike <evwike@gmail.com>
2020-12-16 17:53:33 -0600

    Configured for reproduction

--- app.js
+++ app.js
@@ -3,28 +3,19 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sql = require('mssql');
-
-const router = express.Router();
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
+const DB_CONFIG = require('config').get('db');
+const config = `mssql://${DB_CONFIG.user}:${DB_CONFIG.password}@${DB_CONFIG.server}/${DB_CONFIG.database}?encrypt=true`;
 const app = express();
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
-
-const config = 'mssql://evan:5291Ev!n@umkc-cs470.database.windows.net/cs470?encrypt=true';
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 function send(res, data, err) {
     if (err) {
@@ -95,6 +86,5 @@ app.get('/patient/history/:patientId', function (req, res) {
         });
     });
 })
-
 
 module.exports = app;
---
+++ config/default.js
@@ -0,0 +1,10 @@
+const config = {
+  "db": {
+    "user": "<username>",
+    "password": "<password>",
+    "server": "<server>",
+    "database": "<database>"
+  }
+}
+
+module.exports = config;
